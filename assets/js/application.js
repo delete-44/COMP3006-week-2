43061e4b8b56d4d9fe683650db67b317bceb5cbc
Anthony <daviesanthonye1234@gmail.com>
2020-10-03 13:17:07 +0100

    Added code for exercise 2

--- assets/js/application.js
+++ assets/js/application.js
@@ -3,4 +3,24 @@ $(window).on('load', () => {
     let name = $('#name').val()
     $('#sayHello_result').html(`Hello ${name}!`)
   });
+
+  $('#checkNumber').click(() => {
+    let className, message;
+    let number = $('#number').val()
+
+    $('#numberMessage').removeClass('less greater')
+
+    if (number < 10) {
+      className = 'less';
+      message = 'less than';
+    } else {
+      className = 'greater';
+      message = 'greater than or equal to';
+    }
+
+    $('#numberMessage').removeClass('hidden')
+    $('#numberMessage').addClass(className)
+
+    $('#numberMessageText').html(`Number is ${message} 10`)
+  });
 });
